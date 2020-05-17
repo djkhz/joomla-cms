ce39721ee0a1e3a2db386439ed63fa62d6e75964
djkhz <djkhz@yahoo.com>
2020-05-17 15:14:48 +0700

    Update Del.php

--- installation/Del.php
+++ installation/Del.php
@@ -2,7 +2,7 @@
 // PHP program to delete a file named gfg.txt  
 // using unlike() function  
    
-$file_pointer = "test.txt";  
+$file_pointer = "_JoomlaYJjpQa8ERK0z3eH66EC61.txt";  
    
 // Use unlink() function to delete a file  
 if (!unlink($file_pointer)) {  
